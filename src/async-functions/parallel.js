c824c8ce89ec7caf30c1574427f7ea49f5412207
Rahul Jain <rahul.jain03@sap.com>
2020-03-18 18:37:47 +0530

    async parallel implementation

---
+++ src/async-functions/parallel.js
@@ -0,0 +1,41 @@
+const parallel = function(array, callback) {
+  const length = array.length;
+
+  let index = 0;
+
+  array.forEach(element => {
+    element(function(err, result) {
+      if (err) return err;
+      index++;
+      if (index == length) {
+        callback();
+      }
+    });
+  });
+};
+
+// Simulate async operations by using the setTimeout function with random timeout value. All it does is the print the number passed into the function.
+const asyncTask = function(num) {
+  return function(cb) {
+    setTimeout(function() {
+      console.log(num);
+      cb(null, num);
+    }, Math.random() * 1000);
+  };
+};
+
+// An array of asyncTask
+const tasks = [
+  asyncTask(1),
+  asyncTask(2),
+  asyncTask(3),
+  asyncTask(4),
+  asyncTask(5)
+];
+
+function abc() {
+  console.log("abc");
+}
+
+// run the async tasks in parallel
+parallel(tasks, abc);
