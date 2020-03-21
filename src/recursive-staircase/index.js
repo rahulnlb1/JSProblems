ce75da58028717ec18ed125afaa41170a936ec7b
Rahul Jain <rahul.jain03@sap.com>
2020-03-22 01:15:28 +0530

    updated recusrive staircase with fibonacci

--- src/recursive-staircase/index.js
+++ src/recursive-staircase/index.js
@@ -17,4 +17,14 @@ function staircase(n) {
   return count;
 }
 
-console.log(staircase(3)); //should print 3
+// This lead to fibonacci series as stair n can be reached from either stair(n-1) or stair(n-2).
+// So, number of ways to reach stair n = number of ways to reach start (n-1) + number of ways to reach stair (n-2)
+
+function fib(n) {
+  if (n <= 1) return n;
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+console.log(staircase(5)); //should print 3
+console.log(fib(5)); //should print 3
